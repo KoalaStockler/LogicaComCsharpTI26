dface0b43ffb0f4afb5f22724e1ce83e7df83429
Keno Stockler <koalakeno@gmail.com>
2025-03-26 10:59:03 -0300

    Calculo de idade
    Primeiro programa em C#

---
+++ Calculo_de_Idade/Calculo_de_Idade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_de_Idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano_atual, ano_nascimento, idade;
+            Console.Write("Digite o ano atual: ");
+            ano_atual = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o seu ano de nascimento: ");
+            ano_nascimento = int.Parse(Console.ReadLine());
+
+            idade = ano_atual - ano_nascimento;
+            Console.Write("Idade: " + idade);
+        }
+    }
+}
