8692c0262d3e700216f8671f3f77f92111f268fd
Keno Stockler <koalakeno@gmail.com>
2025-03-27 11:35:12 -0300

    Atividades do portugol refeitas para o C#

---
+++ Números_PARES_(ENQUANTO)/Números_PARES_(ENQUANTO)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Números_PARES__ENQUANTO_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadorPares = 0;
+
+            while (contadorPares <= 100)
+            {
+                if (contadorPares % 2 == 0)
+                {
+                    Console.WriteLine(contadorPares + "\n");
+                }
+                contadorPares++;
+            }
+        }
+    }
+}
