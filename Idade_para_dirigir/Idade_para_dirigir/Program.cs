a2580a5b927f3fd4b8227828889c754e36b8a322
Keno Stockler <koalakeno@gmail.com>
2025-03-28 09:32:45 -0300

    Atividades C#

---
+++ Idade_para_dirigir/Idade_para_dirigir/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idade_para_dirigir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.Write("Digite a idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade < 18)
+            {
+                Console.WriteLine("Não pode dirigir");
+            }
+            else
+            {
+                Console.WriteLine("Pode dirigir");
+            }
+        }
+    }
+}
---
+++ Media_Nota/Media_Nota/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Nota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, nota4, media;
+
+            Console.Write("Digite a nota 1: ");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 2: ");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 3: ");
+            nota3 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 4: ");
+            nota4 = double.Parse(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            if (media >= 7)
+            {
+                Console.WriteLine($"Aprovado com média: {media:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"Reprovado com média: {media:F2}");
+            }
+        }
+    }
+}
