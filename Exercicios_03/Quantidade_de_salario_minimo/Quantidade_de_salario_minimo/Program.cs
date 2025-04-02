0f43f393940ed6d9591bb4ca6df0ba38d3f17cfa
Keno Stockler <koalakeno@gmail.com>
2025-04-02 10:58:23 -0300

    Exercicios em C#

--- Exercicios_03/Quantidade_de_salario_minimo/Quantidade_de_salario_minimo/Program.cs
+++ Exercicios_03/Quantidade_de_salario_minimo/Quantidade_de_salario_minimo/Program.cs
@@ -8,16 +8,14 @@ namespace Quantidade_de_salario_minimo
 {
 
     /*
-     2-	Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário,        calcule quantos salários mínimos esse 
+     2-	Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse 
         usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.518,00).
      */
     internal class Program
     {
         static void Main(string[] args)
         {
-            double salarioMinimo = 1518;
-            double salarioUsuario;
-            double qtdSalarioMinimo;
+            double salarioMinimo = 1518, salarioUsuario, qtdSalarioMinimo;
 
             Console.Write("Digite o salario: ");
             salarioUsuario = double.Parse(Console.ReadLine());
